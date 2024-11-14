f5732958fd5677e078dc4d631531db4821cedf48
TaymaZ <taymazak1382@gmail.com>
2024-11-14 22:28:32 +0330

    feat: navbar Indicator Completed

--- app/components/shared/header/desktop/navbar/index.vue
+++ app/components/shared/header/desktop/navbar/index.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import { LinksHeaderGet } from '~/services/links'
 import { LinksHeaderTypeEnum } from '~/types/links'
-import Indicator from './Indicator.vue'
 import DefaultLink from './types/Default.vue'
 import DropdownLink from './types/Dropdown.vue'
 import MegaMenuLink from './types/MegaMenu.vue'
@@ -16,58 +15,86 @@ const { data } = await useAsyncData(
     },
   },
 )
-const sortedParentLinks = computed(() => {
-  if (data.value?.data) {
-    return [...data.value?.data]
-      .filter(link => link.tn_parent === null)
-      .sort((a, b) => {
-        return a.order - b.order
+
+function getSortedParentLinks(links: any[]) {
+  return links
+    .filter(link => link.tn_parent === null)
+    .sort((a, b) => a.order - b.order)
+}
+
+function getLinksByType(type: LinksHeaderTypeEnum) {
+  return computed(() => {
+    if (data.value?.data) {
+      return getSortedParentLinks(data.value?.data).filter(
+        link => link.link_type === type,
+      )
+    }
+    return []
+  })
+}
+
+// Create computed properties for each link type
+const defaultLinks = getLinksByType(LinksHeaderTypeEnum.DEFAULT)
+const dropdownLinks = getLinksByType(LinksHeaderTypeEnum.DROPDOWN)
+const normalMenuLinks = getLinksByType(LinksHeaderTypeEnum.NORMAL_MENU)
+const megaMenuLinks = getLinksByType(LinksHeaderTypeEnum.MEGA_MENU)
+
+// Indicator Section
+const navWrapper = ref<HTMLElement | null>(null)
+const indicator = ref<HTMLElement | null>(null)
+onMounted(async () => {
+  await nextTick() // Wait for DOM updates
+
+  if (navWrapper.value && indicator.value) {
+    // Select all direct children of the navWrapper
+    const firstLayerChildren = navWrapper.value.children
+
+    // Add mouseenter event listener to each first-layer child
+    Array.from(firstLayerChildren).forEach((child) => {
+      child.addEventListener('mouseenter', (e: Event) => {
+        if (!indicator.value)
+          return
+
+        const target = e.currentTarget as HTMLElement
+        const offsetLeft = target?.offsetLeft
+        const offsetWidth = target?.offsetWidth
+
+        indicator.value.style.transform = `translate3d(${offsetLeft}px, 0, 0)`
+        indicator.value.style.width = `${offsetWidth}px`
+
+        const color = target.dataset.color
+        if (color) {
+          indicator.value.style.backgroundColor = color
+        }
       })
+
+      child.addEventListener('mouseleave', () => {
+        if (!indicator.value)
+          return
+        indicator.value.style.width = '0'
+        indicator.value.style.backgroundColor = '' // Reset background color
+      })
+    })
+
+    // Set initial position for the indicator
+    const firstNavItem = firstLayerChildren[1] as HTMLElement
+    if (firstNavItem && indicator.value) {
+      const offsetLeft = firstNavItem.offsetLeft
+      indicator.value.style.transform = `translate3d(${offsetLeft}px, 0, 0)`
+    }
   }
-  return []
 })
-// Computed property for each link type
-const defaultLinks = computed(() =>
-  sortedParentLinks.value.filter(
-    link => link.link_type === LinksHeaderTypeEnum.DEFAULT,
-  ),
-)
-const dropdownLinks = computed(() =>
-  sortedParentLinks.value.filter(
-    link => link.link_type === LinksHeaderTypeEnum.DROPDOWN,
-  ),
-)
-const normalMenuLinks = computed(() =>
-  sortedParentLinks.value.filter(
-    link => link.link_type === LinksHeaderTypeEnum.NORMAL_MENU,
-  ),
-)
-const megaMenuLinks = computed(() =>
-  sortedParentLinks.value.filter(
-    link => link.link_type === LinksHeaderTypeEnum.MEGA_MENU,
-  ),
-)
 </script>
 
 <template>
-  <nav class="container flex items-center gap-2 ">
-    <Indicator />
+  <nav ref="navWrapper" class="container flex items-center gap-2">
+    <ClientOnly>
+      <div ref="indicator" class="absolute bottom-0 end-0 z-0 h-[0.15625rem] w-0 touch-none rounded-full bg-primary transition-all duration-300 ease-[ease]" />
+    </ClientOnly>
 
-    <MegaMenuLink
-      :items="megaMenuLinks"
-    />
-    <NormalMenuLink
-        :items="normalMenuLinks"
-      />
-    <DefaultLink
-      :items="defaultLinks"
-    />
-    <DropdownLink
-      :items="dropdownLinks"
-    />
+    <MegaMenuLink :items="megaMenuLinks" />
+    <NormalMenuLink :items="normalMenuLinks" />
+    <DefaultLink :items="defaultLinks" />
+    <DropdownLink :items="dropdownLinks" />
   </nav>
 </template>
-
-<style>
-
-</style>
