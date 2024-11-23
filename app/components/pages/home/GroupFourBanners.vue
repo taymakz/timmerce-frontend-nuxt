f7750ed70f7881ee9a0f69f5549f130cb58b13eb
TaymaZ <taymazak1382@gmail.com>
2024-11-23 21:43:56 +0330

    feat: GroupFour-TwoBanners home page Completed

---
+++ app/components/pages/home/GroupFourBanners.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import Skeleton from '~/components/ui/skeleton/Skeleton.vue'
+import type { BannerDetailType } from '~/types/banner'
+
+const { banners } = defineProps<{
+  banners: BannerDetailType[]
+}>()
+const isMounted = useMounted()
+</script>
+
+<template>
+  <section class="container py-10">
+    <div class="grid grid-cols-2 gap-4 lg:grid-cols-4">
+      <template v-if="!isMounted">
+        <Skeleton v-for="_ in 4" :key="_" class="w-full h-[244px] animate-pulse shadow rounded-xl" />
+      </template>
+
+      <NuxtLink  v-for="(banner, index) in banners" :key="index" :to="banner.url" :class="{ 'sr-only': !isMounted }">
+        <NuxtImg placeholder loading="lazy" :src="getMediaUrl(banner.normal_banner)" :alt="banner.title" class="rounded-xl size-full" />
+      </NuxtLink>
+    </div>
+  </section>
+</template>
+
+<style>
+
+</style>
